66293f05a9f3466b77aa1ccdc420fb19ddb1791e
decabeza <alberto@decabeza.es>
2020-05-07 12:06:55 +0200

    Use only dutch language
    Co-Authored-By: Julian Nicolas Herrero <microweb10@gmail.com>

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,8 @@
 module Consul
   class Application < Rails::Application
+    unless Rails.env.test?
+      config.i18n.default_locale = :nl
+      config.i18n.available_locales = ["nl"]
+    end
   end
 end
