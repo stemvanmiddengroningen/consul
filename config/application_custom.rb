b62a92cbb73c2f97eade64b5574194121014015f
Julian Herrero <microweb10@gmail.com>
2020-06-25 10:12:38 +0700

    Add en fallback for nl missing translations

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,8 +1,4 @@
 module Consul
   class Application < Rails::Application
-    unless Rails.env.test?
-      config.i18n.default_locale = :nl
-      config.i18n.available_locales = ["nl"]
-    end
   end
 end
