0f215c2b3c505e8b6bfe0153df71823d1c6f985e
decabeza <alberto@decabeza.es>
2022-07-14 15:19:05 +0200

    Add custom video to poll

---
+++ spec/system/custom/polls_custom_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+describe "Polls custom" do
+  scenario "Poll one shows custom video" do
+    first_poll = create(:poll, id: 1)
+    second_poll = create(:poll)
+    third_poll = create(:poll)
+
+    visit poll_path(first_poll)
+
+    expect(page).to have_selector "#polls_custom_video"
+    expect(page.find(:css, "iframe")[:src]).to eq "https://www.youtube.com/embed/kM04Nt2ehq4"
+
+    visit poll_path(second_poll)
+
+    expect(page).not_to have_selector "#polls_custom_video"
+    expect(page).not_to have_css "iframe"
+
+    visit poll_path(third_poll)
+
+    expect(page).not_to have_selector "#polls_custom_video"
+    expect(page).not_to have_css "iframe"
+  end
+end
