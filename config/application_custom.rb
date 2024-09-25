f51bec368afb209ef210f38b64977b05a4385f88
Alberto <alberto@decabeza.es>
2024-09-25 09:15:29 +0200

    Fixes after upgrade

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,7 @@
 module Consul
   class Application < Rails::Application
+    initializer :exclude_custom_locales_automatic_loading, before: :add_locales do
+      paths.add "config/locales", glob: "**[^custom]*/*.{rb,yml}"
+    end
   end
 end
--- spec/system/custom/polls_custom_spec.rb
+++ spec/system/custom/polls_custom_spec.rb
@@ -8,17 +8,17 @@ describe "Polls custom" do
 
     visit poll_path(first_poll)
 
-    expect(page).to have_selector "#polls_custom_video"
+    expect(page).to have_css "#polls_custom_video"
     expect(page.find(:css, "iframe")[:src]).to eq "https://www.youtube.com/embed/kM04Nt2ehq4"
 
     visit poll_path(second_poll)
 
-    expect(page).not_to have_selector "#polls_custom_video"
+    expect(page).not_to have_css "#polls_custom_video"
     expect(page).not_to have_css "iframe"
 
     visit poll_path(third_poll)
 
-    expect(page).not_to have_selector "#polls_custom_video"
+    expect(page).not_to have_css "#polls_custom_video"
     expect(page).not_to have_css "iframe"
   end
 end
